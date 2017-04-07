c16439eb4b4c76c55d9c5bff7f30476cca9cbeee
hellojol <hellojol@163.com>
2017-04-07 09:23:35 +0800

    12

--- quertzDemo/src/main/java/quertz/demo/core/JonCron.java
+++ quertzDemo/src/main/java/quertz/demo/core/JonCron.java
@@ -17,4 +17,11 @@ public class JonCron {
 	public static final String everyDay2 ="0 0 2 * * ?";//每天早上2点执行一次
 	public static final String everyMonFirst ="0 0 1 1 * ?";//每月1号1点执行一次
 	public static final String everyMonLast ="0 0 23 L * ?";//每月最后一天 23点执行一次
+	/**
+	 * 自定义时间 粒度
+	 */
+	public static final String selfMinute00 ="0 0,15,30,45 * * * ?";//在 0分,15分、30分、45分执行一次：
+	public static final String selfHour00 ="0 0 0,6,12,18 * * ?";//每天的0点、6点、12点、18点都执行一次：
+
+			
 }
