7d2425122d3e206c6e9824c0aa4ea4c01cbcc747
hellojol <hellojol@163.com>
2017-04-10 17:24:07 +0800

    delete other jar in pom.xml

---
+++ SpringCloudDemo/client/src/main/java/demo/cloud/client/SampleController.java
@@ -0,0 +1,23 @@
+package demo.cloud.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class SampleController  {
+ 
+    @ResponseBody
+    @RequestMapping(value = "/")
+    String home() {   
+        return "Hello World!";
+    }
+ 
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class,  "--server.port=8081");
+    }
+ 
+}
